cd87971aa4559aaada71b67094242de72ca3765d
hzzc <info@zhongcesw.com>
2016-06-26 15:54:59 +0800

    Initial Commit

---
+++ TableViewHeaderImageScale/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TableViewHeaderImageScale
+//
+//  Created by hzzc on 16/6/26.
+//  Copyright © 2016年 hzzc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TableViewHeaderImageScale/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TableViewHeaderImageScale
+//
+//  Created by hzzc on 16/6/26.
+//  Copyright © 2016年 hzzc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
